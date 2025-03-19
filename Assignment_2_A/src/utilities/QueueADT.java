9f588cd8d32c02cc3a625ccf7a58dfd2e26cbe65
manminder11 <ManminderSingh@edu.sait.ca>
2025-03-19 14:10:44 -0600

    ADT for both stack and queue

--- Assignment_2_A/src/utilities/QueueADT.java
+++ Assignment_2_A/src/utilities/QueueADT.java
@@ -1,6 +1,53 @@
 package utilities;
 
-public interface QueueADT {
-	
+/**
+ * A generic queue interface that defines how a queue should work.
+ * A queue follows the "First In, First Out" (FIFO) principle.
+ *
+ * @param <T> The type of elements stored in the queue.
+ */
+public interface QueueADT<T> {
 
+    /**
+     * Adds an item to the end of the queue.
+     *
+     * @param element The item to add.
+     * @throws NullPointerException If the element is null.
+     */
+    void enqueue(T element);
+
+    /**
+     * Removes and returns the item from the front of the queue.
+     *
+     * @return The item that was removed.
+     * @throws java.util.NoSuchElementException If the queue is empty.
+     */
+    T dequeue();
+
+    /**
+     * Peeks at the front item without removing it.
+     *
+     * @return The item at the front of the queue.
+     * @throws java.util.NoSuchElementException If the queue is empty.
+     */
+    T first();
+
+    /**
+     * Checks if the queue is empty.
+     *
+     * @return true if the queue has no items, false otherwise.
+     */
+    boolean isEmpty();
+
+    /**
+     * Gets the number of items in the queue.
+     *
+     * @return The count of items in the queue.
+     */
+    int size();
+
+    /**
+     * Clears all items from the queue, making it empty.
+     */
+    void clear();
 }
--- Assignment_2_A/src/utilities/StackADT.java
+++ Assignment_2_A/src/utilities/StackADT.java
@@ -1,6 +1,53 @@
 package utilities;
 
-public interface StackADT {
+/**
+ * A generic stack interface that defines how a stack should work.
+ * A stack follows the "Last In, First Out" (LIFO) principle.
+ *
+ * @param <T> The type of elements stored in the stack.
+ */
+public interface StackADT<T> {
 
-	
+    /**
+     * Adds an item to the top of the stack.
+     *
+     * @param element The item to add.
+     * @throws NullPointerException If the element is null.
+     */
+    void push(T element);
+
+    /**
+     * Removes and returns the item from the top of the stack.
+     *
+     * @return The item that was removed.
+     * @throws java.util.NoSuchElementException If the stack is empty.
+     */
+    T pop();
+
+    /**
+     * Peeks at the top item without removing it.
+     *
+     * @return The item at the top of the stack.
+     * @throws java.util.NoSuchElementException If the stack is empty.
+     */
+    T peek();
+
+    /**
+     * Checks if the stack is empty.
+     *
+     * @return true if the stack has no items, false otherwise.
+     */
+    boolean isEmpty();
+
+    /**
+     * Gets the number of items in the stack.
+     *
+     * @return The count of items in the stack.
+     */
+    int size();
+
+    /**
+     * Clears all items from the stack, making it empty.
+     */
+    void clear();
 }
